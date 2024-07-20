fdacc53271f19f346cfa76f0d9481e30b6918cde
Leroy Oliver <leroyo@hotmail.com>
2024-07-20 20:25:34 +0100

    "start subscription modal"

---
+++ components/SubscribeModal.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { ProductWithPrice } from "@/types";
+import Modal from "./Modal";
+
+interface SubscribeModalProps {
+  products: ProductWithPrice[];
+}
+
+const SubscribeModal: React.FC<SubscribeModalProps> = ({ products }) => {
+  let content = <div className="text-center">No Product Available</div>;
+  return (
+    <Modal
+      title="Only for premium users"
+      description="Listen to music with Spotify Premium"
+      isOpen
+      onChange={() => {}}
+    >
+      {content}
+    </Modal>
+  );
+};
+
+export default SubscribeModal;
